dcdac143f267981d0bc256275bc4e09afaf31eac
Devynne Marshall <devynnem@gmail.com>
2023-07-25 18:34:52 -0600

    feat:Inital commit

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+import './App.css';
+
+function App() {
+  const [articles, setArticles] = useState([]);
+  const [singleArticle, setSingleArticle] = ({});
+  const [filteredArticle, setFilteredArticle] = ([]);
+
+}
+
+
+export default App;
