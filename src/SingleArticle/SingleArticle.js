791202e757a9a6a4abcff4cbb6053c85a874cc0b
Devynne Marshall <devynnem@gmail.com>
2023-07-26 21:01:06 -0600

    feat:add styling to all components

--- src/SingleArticle/SingleArticle.js
+++ src/SingleArticle/SingleArticle.js
@@ -1,6 +1,7 @@
 import './SingleArticle.css';
 import React, {useState, useEffect} from 'react';
 import { formatReadableDate } from '../ArticleDetails/ArticleDetails';
+import noImage from '../noImage.png';
 
 
 const SingleArticle = ({ article }) => {
@@ -10,7 +11,13 @@ const SingleArticle = ({ article }) => {
       <h2>{article.title}</h2>
       <p>{article.description}</p>
       <p>Published on: {formatReadableDate(article.publishedAt)}</p>
-      <img src={article.urlToImage} alt='Article image' />
+      <span>
+          {article.urlToImage ? (
+            <img className='single-image' src={article.urlToImage} alt="Article Image"/>
+          ) : (
+          <img className="no-card-img" src={noImage} alt="no image" />
+          )}
+      </span>
       <p>{article.content}</p>
       <a href={article.url} target='_blank' rel='noopener noreferrer'>
         Read more
